6573e01b6c841cb298beaa09724032d7e5f78d8d
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2023-06-18 16:26:53 -0700

    chore: add utility functions to Init.lua for creating and retrieving addon tables

--- Init/Init.lua
+++ Init/Init.lua
@@ -1,3 +1,23 @@
-local AddonName, _ = ...
+local ipairs = ipairs
+
+local AddonName, Private = ...
+
+function Private:createTable(keys)
+	for _, key in ipairs(keys) do
+		self[key] = self[key] or {}
+		self = self[key]
+	end
+	return self
+end
+
+function Private:getTable(keys)
+	for _, key in ipairs(keys) do
+		if not self[key] then
+			return nil
+		end
+		self = self[key]
+	end
+	return self
+end
 
 LibStub("AceAddon-3.0"):NewAddon(AddonName)
