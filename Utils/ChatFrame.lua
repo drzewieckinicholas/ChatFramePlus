fb1ba4014fb4ddec7655570136a40cfb165c99d5
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2023-06-18 16:59:58 -0700

    chore: add new chat frame utility functions

--- Utils/ChatFrame.lua
+++ Utils/ChatFrame.lua
@@ -11,3 +11,21 @@ function ChatFrameUtils.forEachChatFrame(callback)
 		end
 	end
 end
+
+function ChatFrameUtils.getChatFrameName(chatFrame)
+	return chatFrame and chatFrame:GetName()
+end
+
+function ChatFrameUtils.getChatTab(chatFrame)
+	return chatFrame and _G[chatFrame:GetName() .. "Tab"]
+end
+
+function ChatFrameUtils.getChatTabName(chatFrame)
+	if not chatFrame then
+		return
+	end
+
+	local chatTab = ChatFrameUtils.getChatTab(chatFrame)
+
+	return chatTab and chatTab:GetText()
+end
