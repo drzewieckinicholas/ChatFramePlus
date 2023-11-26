a277286f8a816e0ff2101c15cdd13709ec1e00ee
Nicholas Drzewiecki <79545110+drzewieckinicholas@users.noreply.github.com>
2023-11-26 00:21:58 -0800

    feat: increase max font size and add font for dyslexic users (#14)

--- Constants/Font.lua
+++ Constants/Font.lua
@@ -8,7 +8,7 @@ local FontConstants = {}
 FontConstants.SIZE_MIN = 12
 
 --- @type number
-FontConstants.SIZE_MAX = 18
+FontConstants.SIZE_MAX = 24
 
 --- @type number
 FontConstants.SIZE_STEP = 2
@@ -17,6 +17,8 @@ FontConstants.SIZE_STEP = 2
 FontConstants.NAMES = {
 	["Fonts\\ARIALN.TTF"] = "Arial Narrow",
 	["Fonts\\FRIZQT__.TTF"] = "Friz Quadrata TT",
+	["Interface\\AddOns\\ChatFramePlus\\Assets\\Fonts\\OpenDyslexic3-Regular.ttf"] = "OpenDyslexic 3",
+	["Interface\\AddOns\\ChatFramePlus\\Assets\\Fonts\\OpenDyslexicMono-Regular.otf"] = "OpenDyslexic Mono",
 }
 
 --- @type table<string, string>
