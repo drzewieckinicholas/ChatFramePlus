bbfb5e054d31b46a7f052b645375c95dfd8a606c
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2023-12-19 22:20:54 -0800

    fix(db): invalid database index error dependent on New Whispers option
    If a user sets the option New Whispers to Both or New Tab and receives or sends a whisper, a new
    chat frame is created. No table in the database exists for the new chat frame because a table is
    only created for the chat frames that exist when the addon first loads. This fix creates a table in
    the database for new chat frames when GetChatFramesTable gets called.
    
    issue #15

--- Utils/Database.lua
+++ Utils/Database.lua
@@ -21,6 +21,10 @@ function DatabaseUtils.GetChatFramesTable(index, key)
 	assert(type(index) == "number", "bad argument #1 expected number got " .. type(index))
 	assert(type(key) == "string", "bad argument #2 expected string got " .. type(key))
 
+	if not Private.db.profile.chatFrames[index] then
+		Private.db.profile.chatFrames[index] = Private.Database.GetChatFrameDefaults()
+	end
+
 	return Private.db.profile.chatFrames[index][key]
 end
 
